413b7dbfaddb471d8518a4becec85c8d21473db4
Jakob Prossinger <jakob.prossinger@gmail.com>
2021-11-13 19:13:15 +0100

    reading ds18b20 sensors

--- run.py
+++ run.py
@@ -2,11 +2,23 @@
 from os import name
 from src.sensors.ds18b20 import DS18B20
 
-DEVICE_ADRESS  = '28-00000cdfc36f'
+TEMPSENSORS_DEVICE_ADRESSES = ['28-00000cdfc36f', '28-00000cdf6b81']
+
+def getTempSensorList(ds18b20_devices):
+    temperature_list = [0.0] * len(ds18b20_devices)
+    flag = 0
+    for device in ds18b20_devices, :
+        temperature_list[flag] = ds18b20_devices[flag].getTemperature()
+        flag += 1
+    return temperature_list
 
 def main():
-    ds18b20A = DS18B20(DEVICE_ADRESS)
-    print(ds18b20A.getTemperature())
+    #init ds18b20 Temperature Sensors
+    ds18b20_sensors = []
+    for device_address in TEMPSENSORS_DEVICE_ADRESSES:
+        ds18b20_sensors.append(DS18B20(device_address))
+    #read ds18b20 Temperature Sensors
+    print(getTempSensorList(ds18b20_sensors))
 
 if __name__ == '__main__':
     main()
