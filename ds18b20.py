b27d4fa03947581d972d66b785a3396bd069ca43
Jakob Prossinger <jakob.prossinger@gmail.com>
2021-11-13 18:17:52 +0100

    added ds18b20 source file

---
+++ ds18b20.py
@@ -0,0 +1,30 @@
+from os import open
+import sys
+
+class ds18b20:
+
+    _DS18B20_DEFAULT_DEVICE_ADDRESS = "28-00000cdfc36f"
+    _DS18B20_FOLDER_STRUCTURE1 = '/sys/bus/w1/devices/'
+    _DS18B20_FOLDER_STRUCTURE2 = '/w1_slave'
+
+        #Constructor
+    def __init__(self, device_address=_DS18B20_DEFAULT_DEVICE_ADDRESS):
+        self.device_adress = device_address
+        self.folder_structure = self._DS18B20_FOLDER_STRUCTURE1 + self.device_adress + self._DS18B20_FOLDER_STRUCTURE2
+
+    def getTemperature(self):
+        try:
+            file = open(self.folder_structure)
+        except OSError:
+            print('cannot open', (self.folder_structure))
+            return 'Error'
+        else:
+            filecontent = file.read()
+            file.close()
+
+			# Error in Temperatursensor Daten
+            if len(filecontent) != 75:
+                print('Error')
+            else:
+                stringvalue = filecontent.split("\n")[1].split(" ")[9]
+                return float(stringvalue[2:]) / 1000
