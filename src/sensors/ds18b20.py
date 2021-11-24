4ad4ae6a414903d3e66fe483cdb8387b49cde175
Jakob Prossinger <jakob.prossinger@gmail.com>
2021-11-24 10:21:06 +0100

    changed variable name

--- src/sensors/ds18b20.py
+++ src/sensors/ds18b20.py
@@ -4,17 +4,17 @@ import sys, os
 class DS18B20:
 
     _DS18B20_DEFAULT_DEVICE_ADDRESS = "28-00000cdfc36f"
-    _DS18B20_FOLDER_STRUCTURE1 = '/sys/bus/w1/devices/'
-    _DS18B20_FOLDER_STRUCTURE2 = '/w1_slave'
+    _DS18B20_DIRECTORY1 = '/sys/bus/w1/devices/'
+    _DS18B20_DIRECTORY2 = '/w1_slave'
 
     #Constructor
     def __init__(self, device_address=_DS18B20_DEFAULT_DEVICE_ADDRESS):
         self.device_adress = device_address
-        self.folder_structure = self._DS18B20_FOLDER_STRUCTURE1 + self.device_adress + self._DS18B20_FOLDER_STRUCTURE2
+        self.directory = self._DS18B20_DIRECTORY1 + self.device_adress + self._DS18B20_DIRECTORY2
 
     def getTemperature(self):
         try:
-            file = open(self.folder_structure, 'r')
+            file = open(self.directory, 'r')
         except OSError:
             return 'Sensor Missing'
         else:
