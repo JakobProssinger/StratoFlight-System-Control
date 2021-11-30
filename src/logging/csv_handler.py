be797e92847edeae5522f5300568a7746cd9e1ba
Jakob Prossinger <jakob.prossinger@gmail.com>
2021-11-30 15:57:15 +0100

    open file now with >with open<

--- src/logging/csv_handler.py
+++ src/logging/csv_handler.py
@@ -1,39 +1,38 @@
 import csv
 from os import write
 
-class CSV_HANDLER:
 
+class CSV_HANDLER:
     def __init__(self, csv_handler_directory: str, header_list: list) -> None:
         self.directory = csv_handler_directory
         self.header_list = header_list
 
     def csv_write_header(self) -> None:
         try:
-            with open(self.directory, 'w', newline = '') as csvfile:
-                writer = csv.writer(csvfile, delimiter=',')
+            with open(self.directory, 'w', newline='') as csvfile:
+                writer = csv.writer(csvfile, delimiter=';')
                 writer.writerow(self.header_list)
         except Exception as e:
             print(e)
 
     def csv_write_data_row(self, data_list: list) -> None:
         try:
-            with open(self.directory, 'a', newline = '') as csvfile:
-                writer = csv.writer(csvfile, delimiter=',')
+            with open(self.directory, 'a', newline='') as csvfile:
+                writer = csv.writer(csvfile, delimiter=';')
                 writer.writerow(data_list)
         except Exception as e:
             print(e)
-    
+
     def csv_write_data_cell(self, data_cell: float) -> None:
         try:
             with open(self.directory, 'a', newline='') as csvfile:
-                csvfile.write(str(data_cell) + ',')
+                csvfile.write(str(data_cell) + ';')
         except Exception as e:
             print(e)
 
-
     def csv_write_newline(self) -> None:
         try:
-            with open(self.directory, 'a', newline = '') as csvfile:
+            with open(self.directory, 'a', newline='') as csvfile:
                 csvfile.write('\r')
         except Exception as e:
             print(e)
