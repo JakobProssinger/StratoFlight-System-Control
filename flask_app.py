e701e102aa093baf68cd264cada5d5745f88a266
Jakob Prossinger <jakob.prossinger@gmail.com>
2021-12-07 15:16:05 +0100

    please dont ask

--- flask_app.py
+++ flask_app.py
@@ -1,24 +1,8 @@
 from flask import Flask, stream_with_context, request, Response, flash
 from flask import render_template
-from src.sensors import ds18b20
-from src.sensors import ina260
 from src.sensors import internal
-from src.logging.csv_handler import CSVHandler
-from src.sensors.sensor_process import SensorObject
+
 import time
-
-CSV_DIRECTORY = './Logging-Files/sensor_data.csv'
-TEMPSENSORS_DEVICE_ADDRESSES = ['28-00000cdfc36f']  #, '28-00000cdf6b81']
-INA260_DEVICE_ADDRESSES = [0x40, 0x41]
-HEADER_LIST = [
-    'TIME', 'INA CURRENT 1/mA', 'INA CURRENT 2/mA', 'INA VOLTAGE 1/mV',
-    'INA VOLTAGE 2/mV', 'ds18b28 Temperature', 'raspberry temperature'
-]
-
-csv_handler = CSVHandler(CSV_DIRECTORY, HEADER_LIST)
-csv_handler.csv_write_data_row(HEADER_LIST)
-sensors_processor = SensorObject(INA260_DEVICE_ADDRESSES,
-                                 TEMPSENSORS_DEVICE_ADDRESSES, csv_handler)
 
 app = Flask(__name__)
 
