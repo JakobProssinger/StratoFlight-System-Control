621c8f494e13c6aa674cd3e43a0eedd51c6a6b01
Jakob Prossinger <jakob.prossinger@gmail.com>
2021-12-14 16:27:55 +0100

    remove: unnecessary logging settings

--- src/sensors/internal.py
+++ src/sensors/internal.py
@@ -2,14 +2,6 @@ import os
 import logging
 
 logger = logging.getLogger("strato_logger.cpu_temperature_sensor")
-logger.setLevel(logging.ERROR)
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)-12s - %(levelname)s:\n\tat %(funcName)s() - %(message)s'
-)
-file_Handler = logging.FileHandler('./Logging-Files/sensor_data.csv')
-file_Handler.setLevel(logging.ERROR)
-file_Handler.setFormatter(formatter)
-logger.addHandler(file_Handler)
 
 
 def get_raspberry_temperature() -> float:
