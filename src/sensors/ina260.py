21b1f8132c94b30df196f44017b0cff125e2a006
Jakob Prossinger <jakob.prossinger@gmail.com>
2021-12-14 17:52:15 +0100

    DOC: added DOC to ina260

--- src/sensors/ina260.py
+++ src/sensors/ina260.py
@@ -1,13 +1,89 @@
+"""
+@File:          ina260.py
+@Descrption:    a module read and write data from the Adafruit INA260 chip
+@Author:        Prossinger Jakob
+@Date:          14 December 2021
+@Todo:          * translate the read config register
+"""
 from typing import Sized
 import smbus
 import logging
 
 # setup for logging
-ina_module_logger = logging.getLogger("strato_logger.sensor_process.ina260")
+ina_module_logger = logging.getLogger("strato_logger.sensor_process.INA260")
 
 
 #source: https://github.com/charkster/INA260/tree/077521eded5c8efe22bf843dfd2fa462e10bb9c5
 class INA260:
+    """
+    A class used to read and write data from the Adafruit
+    Ina260 chip
+
+    ...
+
+    Attributes
+    ----------
+    self.i2c : smbus
+        opens a i2c bus for the INA260
+
+    self.device_address : int (hex)
+        I2C Address of the INA260
+    
+    self.logger : logger
+        logger of the ina260
+
+    ina_module_logger : Logger
+        a logger from the standard logger module. Child logger from strato_logger
+
+    _INA260_DEFAULT_DEVICE_ADDRESS: int (hex)
+        Default Address of the Adafruit INA260.
+
+    _INA260_CONFIG_ADDR : int(hex)
+        Address of the config register of the Adafruit INA260.
+
+    _INA260_CURRENT_ADDR : int (hex)
+        Address of the electrical current register of the Adafruit INA260.
+
+    _INA260_BUS_VOLTAGE_ADDR : int (hex)
+        Address of the electrical voltage register of the Adafruit INA260.
+
+    _INA260_BUS_VOLTAGE_LSB : double
+        voltage size of the least significant bit of the voltage register
+        of the Adafruit INA260, in mV.
+
+    _INA260_CURRENT_LSB : double
+        electrical current size of the least significant of the current register 
+        bit of the Adafruit INA260, in mA
+
+    Methods
+    -------
+    twos_compliment_to_int(self, val: int, len: int) -> int
+        returns the integer of a twos_compilemt number, based on the legth.
+    
+    read_ina(self, register_address: int, register_size: int) -> float
+        returns the values from an INA260 register
+    
+    write_ina(self, register_address: int, byte_list: int) -> bool
+        writes a list of bytes on an INA260-Address.
+        If succesful return true
+
+    get_bus_voltage(self) -> float
+        reads and returns the voltage from the voltage register in
+        the INA260 object. If successful return a float int mV else return "noVoltage"
+
+    get_current(self) -> float
+        reads and returns the current from the current register in
+        the INA260 object. If successful return a float in mA else return "noCurrent"
+    
+    reset_chip() -> None
+        resets the config register of the INA260 to default.
+
+    read_configuration_register(self) -> int:
+        reads and returns the configregister of the INA260
+
+    def activate_average(self, samples: int) -> None:
+        activates the averaging on the INA260 based on the samples.
+    """
 
     _INA260_DEFAULT_DEVICE_ADDRESS = 0x40
     _INA260_CONFIG_ADDR = 0x00
@@ -19,6 +95,13 @@ class INA260:
     # Constructor
     def __init__(self,
                  device_address: int = _INA260_DEFAULT_DEVICE_ADDRESS) -> None:
+        """
+        Parameters
+        ----------
+        device_address : int (hex)
+            the i2c address of the INA260 
+        """
+
         self.i2c = smbus.SMBus(1)  #/dev/i2c-1
         self.device_address: int = device_address
         self.logger = logging.getLogger(
@@ -26,12 +109,46 @@ class INA260:
         self.logger.info("created ina260 object")
 
     def twos_compliment_to_int(self, val: int, len: int) -> int:
+        """
+        Description
+        ----------
+        returns the integer of a twos_compilemt number, based on the legth.
+
+        Parameters
+        ----------
+        val : int 
+            integer value of a tows compliment number 
+        
+        len : int
+            length of the integer number         
+        """
         # Convert twos compliment to integer
         if (val & (1 << len - 1)):
             val = val - (1 << len)
         return val
 
     def read_ina(self, register_address: int, register_size: int) -> float:
+        """
+        Description
+        ----------
+        returns the values from an INA260 register
+
+        Parameters
+        ----------
+        register_address : int 
+            integer value of the i2c address  
+        
+        register_size : int
+            size of the register in bytes
+
+        Throws
+        ------
+        OSException:
+            if no I2C could could be made.
+
+        Exception:
+            if something else happends.
+        """
         try:
             return self.i2c.read_i2c_block_data(self.device_address,
                                                 register_address,
@@ -45,6 +162,28 @@ class INA260:
         return ["Error"] * register_size
 
     def write_ina(self, register_address: int, byte_list: int) -> bool:
+        """
+        Description
+        ----------
+        writes a list of bytes on an INA260-Address.
+        If succesful return true
+
+        Parameters
+        ----------
+        register_address : int 
+            integer value of the i2c address  
+        
+        byte_list : int
+            integer list with bytes to write on the i2c address
+
+        Throws
+        ------
+        OSException:
+            if no I2C could could be made.
+
+        Exception:
+            if something else happends.
+        """
         try:
             self.i2c.write_i2c_block_data(self.device_address,
                                           register_address, byte_list)
@@ -59,6 +198,12 @@ class INA260:
         return True
 
     def get_bus_voltage(self) -> float:
+        """
+        Description
+        ----------
+        reads and returns the voltage from the voltage register in
+        the INA260 object. If successful return a float int mV else return "noVoltage"
+        """
         raw_read = self.read_ina(self._INA260_BUS_VOLTAGE_ADDR, 2)
         if type(raw_read[0]) != int:
             return "noVoltage"
@@ -67,6 +212,10 @@ class INA260:
         return vbus
 
     def get_current(self) -> float:
+        """
+        reads and returns the current from the current register in
+        the INA260 object. If successful return a float in mA else return "noCurrent"
+        """
         raw_read = self.read_ina(self._INA260_CURRENT_ADDR, 2)
         if type(raw_read[0]) != int:
             return "noCurrent"
@@ -82,13 +231,29 @@ class INA260:
         return current
 
     def reset_chip(self) -> None:
+        """
+        resets the config register of the INA260 to default.
+        """
         byte_list = [0x80, 0x00]  # reset code for INA260
         self.write_ina(self._INA260_CONFIG_ADDR, byte_list)
 
     def read_configuration_register(self) -> int:
+        """
+        reads and returns the configregister of the INA260
+        """
         return self.read_ina(self._INA260_CONFIG_ADDR, 2)
 
     def activate_average(self, samples: int) -> None:
+        """
+        Description
+        ----------
+        activates the averaging on the INA260 based on the samples.
+
+        Parameters
+        ----------
+        samples : int 
+            sampels to average
+        """
         byte_list = [0x61, 0x27]
         switch = {
             1: 0x061 + (0b000 << 1),
