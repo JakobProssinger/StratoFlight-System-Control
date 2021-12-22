7d0f98f94dd41c23177529bd89feb2f205583406
Jakob Prossinger <jakob.prossinger@gmail.com>
2021-12-22 10:59:51 +0100

    ADDED: activate average at ina260, and print minimum Voltage

--- 01_sensor_tests.py
+++ 01_sensor_tests.py
@@ -4,7 +4,6 @@ from src.sensors.ina260 import INA260
 import src.sensors.internal
 import time
 
-
 TEMPSENSORS_DEVICE_ADDRESSES = ['28-00000cdfc36f', '28-00000cdf6b81']
 INA260_DEVICE_ADDRESSES = [0x40, 0x41]
 
@@ -31,11 +30,24 @@ def get_Bus_Voltage_INA_List(ina260_devices):
 
 
 def main():
+    #setup
+    #init ds18b20 Temperature Sensors
+    ds18b20_sensors = []
+    for device_address in TEMPSENSORS_DEVICE_ADDRESSES:
+        ds18b20_sensors.append(DS18B20(device_address))
+
+    #init ina260 Sensors
+    ina260_sensors = []
+    for device_address in INA260_DEVICE_ADDRESSES:
+        temp_ina260 = INA260(device_address)
+        temp_ina260.reset_chip()
+        temp_ina260.activate_average(4)
+        ina260_sensors.append(temp_ina260)
+
+    min_Voltage = 10000.0  #mV
+    voltage_List = [0.0, 0.0]
     while 1:
-        #init ds18b20 Temperature Sensors
-        ds18b20_sensors = []
-        for device_address in TEMPSENSORS_DEVICE_ADDRESSES:
-            ds18b20_sensors.append(DS18B20(device_address))
+
         #read ds18b20 Temperature Sensors
         print("Outer Temperature: ", getTempSensorList(ds18b20_sensors))
 
@@ -43,17 +55,16 @@ def main():
         print("Internal Temperature: ",
               src.sensors.internal.get_raspberry_temperature())
 
-        #init ina260 Sensors
-        ina260_sensors = []
-        for device_address in INA260_DEVICE_ADDRESSES:
-            temp_ina260 = INA260(device_address)
-            temp_ina260.reset_chip()
-            ina260_sensors.append(temp_ina260)
         #read ina260 current sensors
         print("Bus Current: ", get_Current_INA_List(ina260_sensors))
         #read ina260 bus voltage sensors
-        print("Bus Voltage: ", get_Bus_Voltage_INA_List(ina260_sensors), "\n")
-        time.sleep(2)
+        voltage_List = get_Bus_Voltage_INA_List(ina260_sensors)
+
+        print("Bus Voltage: ", voltage_List)
+        if min_Voltage > voltage_List[1]:
+            min_Voltage = voltage_List[1]
+        print("Min Voltage: " + str(min_Voltage) + "\n")
+        time.sleep(0.5)
 
 
 if __name__ == '__main__':
