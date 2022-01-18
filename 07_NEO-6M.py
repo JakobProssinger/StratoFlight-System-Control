705bd884347691b80828b637085509c7c6e5522d
Jakob Prossinger <jakob.prossinger@gmail.com>
2022-01-18 16:48:04 +0100

    ADDED: GPS Sensor Test, if not connectd error

--- 07_NEO-6M.py
+++ 07_NEO-6M.py
@@ -2,23 +2,35 @@ import serial
 import time
 import string
 import pynmea2
+import atexit
 
 port = "/dev/ttyAMA0"
+
+
+'''
+*Anmkerungen: ohne Antenne beides 0                                                                                                                                                                                                                                                                                                                                                                           
+'''
+@atexit.register
+def atexit_function() -> None:
+    ser.close()
+    print("closed")
+
 ser = serial.Serial(port, baudrate=9600, timeout=0.5)
-dataout = pynmea2.NMEAStreamReader()
+
 try:
     while True:
+        dataout = pynmea2.NMEAStreamReader()
         newdata = ser.readline()
-        if newdata[0:6] == b'$GPRMC':
-            try:
-
-                newmsg = pynmea2.parse(str(newdata))
-                lat = newmsg.latitude
-                lng = newmsg.longitude
-                gps = "Latitude=" + str(lat) + "and Longitude=" + str(lng)
-                print(gps)
-            except Exception as e:
-                print("ERROR: No Signal")
+        #print(str(newdata[0:6]))
+        if str(newdata[0:6]) == "b'$GPRMC'":
+            newmsg = pynmea2.parse(str(newdata)[2:-5])
+            lat = newmsg.latitude
+            lng = newmsg.longitude
+            gps = str(time.asctime()) +"  Latitude=" + str(lat) + " and Longitude = " 
+            + str(lng)                                             
+            print(gps)
+       # ser.close()
 except KeyboardInterrupt:
     ser.close()
+    ser.exit()
     print("closed")
