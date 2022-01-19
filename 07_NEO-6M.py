a47b7fa271b3138b1bcc9e85c152f58bfe83b0f4
Jakob Prossinger <jakob.prossinger@gmail.com>
2022-01-19 09:13:19 +0100

    ADDED: GPS Sensor test

--- 07_NEO-6M.py
+++ 07_NEO-6M.py
@@ -1,8 +1,14 @@
+"""
+@File:          07_NEO-6M.py
+@Descrption:    starts flask server for Strato Flight 2021/2022
+@Author:        Prossinger Jakob
+@Date:          19 January 2021
+@Todo:          * 
+"""
 import serial
-import time
-import string
 import pynmea2
 import atexit
+import os
 
 port = "/dev/ttyAMA0"
 
@@ -10,27 +16,45 @@ port = "/dev/ttyAMA0"
 '''
 *Anmkerungen: ohne Antenne beides 0                                                                                                                                                                                                                                                                                                                                                                           
 '''
+
+
 @atexit.register
 def atexit_function() -> None:
-    ser.close()
+    # ser.close()
     print("closed")
 
-ser = serial.Serial(port, baudrate=9600, timeout=0.5)
 
-try:
-    while True:
-        dataout = pynmea2.NMEAStreamReader()
-        newdata = ser.readline()
-        #print(str(newdata[0:6]))
-        if str(newdata[0:6]) == "b'$GPRMC'":
-            newmsg = pynmea2.parse(str(newdata)[2:-5])
-            lat = newmsg.latitude
-            lng = newmsg.longitude
-            gps = str(time.asctime()) +"  Latitude=" + str(lat) + " and Longitude = " 
-            + str(lng)                                             
-            print(gps)
-       # ser.close()
-except KeyboardInterrupt:
+def read_NEO6M() -> list:
+    ser = serial.Serial(port, baudrate=9600, timeout=0.2)
+    #dataout = pynmea2.NMEAStreamReader()
+    try:
+        for i in range(0, 10):
+            print(i)
+
+            newdata = ser.readline()
+            if str(newdata[0:6]) == "b'$GPRMC'":
+                newmsg = pynmea2.parse(str(newdata)[2:-5])
+                lat = newmsg.latitude
+                lng = newmsg.longitude
+                gps = " Latitude = " + \
+                    str(lat) + " and Longitude = " + str(lng)
+                ser.close()
+                return [lat, lng]
+    except KeyboardInterrupt:
+        ser.close()
+        print("closed")
+    except Exception as e:
+        ser.close()
+        print(e)
     ser.close()
-    ser.exit()
-    print("closed")
+    return [0, 0]
+
+
+def main():
+    print(read_NEO6M())
+    print("-" * 20)
+    print(read_NEO6M())
+
+
+if __name__ == "__main__":
+    main()
