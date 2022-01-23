0eba7efbe0b57689bee5312e96932f73e4431f82
Jakob Prossinger <jakob.prossinger.com>
2022-01-23 11:59:09 +0100

    ADDED: create csv file if does not exist

--- csv_handler/csv_handler.py
+++ csv_handler/csv_handler.py
@@ -2,11 +2,8 @@
 @File:          csv_handler.py
 @Descrption:    a module to handle csv writing
 @Author:        Prossinger Jakob
-@Date:          14 December 2021
+@Date:          23 January 2022
 @Todo:          * change prints to loggs
-                * throw OSErrors insteed of standard excpetions
-                * csv_write_header only if file is empty
-                * test if csv does not exist
 """
 import csv
 import os
@@ -16,25 +13,35 @@ import logging
 class CSV_HANDLER:
     def __init__(self, path: str) -> None:
         self.path = path
+        if os.path.exists(path) is False:
+            with open(path, 'w') as f:
+                f.write('')
+
 
     def csv_write_data_row(self, data_list: list) -> None:
         try:
             with open(self.path, 'a', newline='') as csvfile:
                 writer = csv.writer(csvfile, delimiter=';')
                 writer.writerow(data_list)
+        except OSError:
+            print(OSError)
         except Exception as e:
-            csv_module_logger.error(e)
+            print(e)
 
     def csv_write_data_cell(self, data_cell: str) -> None:
         try:
             with open(self.path, 'a', newline='') as csvfile:
                 csvfile.write(str(data_cell) + ';')
+        except OSError:
+            print(OSError)
         except Exception as e:
-            csv_module_logger.error(e)
+            print(e)
 
     def csv_write_newline(self) -> None:
         try:
             with open(self.path, 'a', newline='') as csvfile:
                 csvfile.write('\r')
+        except OSError:
+            print(OSError)
         except Exception as e:
-            csv_module_logger.error(e)
+            print(e)
