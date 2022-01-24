1fc1c76f77e91d7ed570105547b0a16efdab43e7
Jakob Prossinger <jakob.prossinger@gmail.com>
2022-01-24 20:48:58 +0100

    CHANGED: file header to correct date

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 @File:          config.py
 @Descrption:    configure file for Strato FLight 2021/2022
 @Author:        Prossinger Jakob
-@Date:          23 January 2022
+@Date:          24 January 2022
 @Todo:          *
 """
 import RPi.GPIO as GPIO
