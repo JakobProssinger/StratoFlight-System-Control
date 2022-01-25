22a5e4319fcc9dbb7d7b7bd0c8f04906c4b1267b
Jakob Prossinger <jakob.prossinger.com>
2022-01-25 12:42:49 +0100

    DOC: added docstring

--- sensor/sensor_data/sensor_data.py
+++ sensor/sensor_data/sensor_data.py
@@ -10,6 +10,15 @@ from dataclasses import dataclass
 
 @dataclass
 class sensor_data:
+    """
+    class to store data of sensors
+
+    Parameter:
+        data_name (list): list with all names of the data from the sensor measures
+        data_value (list): list with all measured values from the sensor 
+        data_unit(list): list with all units of the measured values from the sensor
+        data_length(list): lenght of the list above
+    """
     data_name: list
     data_value: list
     data_unit: list
