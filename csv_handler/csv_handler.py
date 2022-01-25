ee2e782481f466e5446f63109c37854b3f2f385a
Jakob Prossinger <jakob.prossinger.com>
2022-01-25 12:47:28 +0100

    DOC: added docstring

--- csv_handler/csv_handler.py
+++ csv_handler/csv_handler.py
@@ -2,7 +2,7 @@
 @File:          csv_handler.py
 @Descrption:    a module to handle csv writing
 @Author:        Prossinger Jakob
-@Date:          24 January 2022
+@Date:          25 January 2022
 @Todo:          * change prints to loggs
 """
 import csv
@@ -11,13 +11,29 @@ import logging
 
 
 class CSV_HANDLER:
+    """
+    class to handle csv writing
+    """
+
     def __init__(self, path: str) -> None:
+        """
+        init function of the CSV_HANDLER class
+
+        Args:
+            path (str): path to the csv-file write
+        """
         self.path = path
         if os.path.exists(path) is False:
             with open(path, 'w') as f:
                 f.write('')
 
     def csv_write_list(self, data_list: list) -> None:
+        """
+        write a list of data to the csv-file with no newline
+
+        Args:
+            data_list (list): list of data to write to the csv-file
+        """
         try:
             with open(self.path, 'a', newline='') as csvfile:
                 for data_point in data_list:
@@ -28,6 +44,12 @@ class CSV_HANDLER:
             print(e)
 
     def csv_write_data_row(self, data_list: list) -> None:
+        """
+        write a list of data to the csv-file with newline
+
+        Args:
+            data_list (list): list of data to write to the csv-file
+        """
         try:
             with open(self.path, 'a', newline='') as csvfile:
                 writer = csv.writer(csvfile, delimiter=';')
@@ -38,6 +60,12 @@ class CSV_HANDLER:
             print(e)
 
     def csv_write_data_cell(self, data_cell: str) -> None:
+        """
+        write single element to the csv-file
+
+        Args:
+            data_cell (str): data to write
+        """
         try:
             with open(self.path, 'a', newline='') as csvfile:
                 csvfile.write(str(data_cell) + ';')
@@ -47,6 +75,9 @@ class CSV_HANDLER:
             print(e)
 
     def csv_write_newline(self) -> None:
+        """
+        write a newline symbol to the csv-file
+        """
         try:
             with open(self.path, 'a', newline='') as csvfile:
                 csvfile.write('\r')
