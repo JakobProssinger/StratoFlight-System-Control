0625ab261a7dda8e1ddc36bb14a535b9ff28e343
JakobProssinger <jakob.prossinger@gmail.com>
2022-03-05 16:28:29 +0100

    ADDED: basic setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+"""Package configuration."""
+from setuptools import find_packages, setup
+
+setup(
+    name="strato-flight",
+    packages=find_packages(
+        where="/home/pi/Documents/StratoFlight-System-Control"),
+
+)
